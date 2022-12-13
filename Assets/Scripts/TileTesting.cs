484354d364a6c1bd68bfe67986dc4be952e8981b
Cláuvin <Clauvin@users.noreply.github.com>
2022-12-13 16:34:27 -0300

    Created and used TileTesting class, that adds one tile to the screen

---
+++ Assets/Scripts/TileTesting.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileTesting : MonoBehaviour
+{
+    public Tile tileTest;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Tilemap>().SetTile(new Vector3Int(0, 0, 0), tileTest);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
