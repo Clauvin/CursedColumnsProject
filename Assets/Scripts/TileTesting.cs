80a744e0cdf372e4cd2e2791d468cbbe537896ed
Cláuvin <Clauvin@users.noreply.github.com>
2022-12-13 16:58:23 -0300

    Adding more blocks to the grid

--- Assets/Scripts/TileTesting.cs
+++ Assets/Scripts/TileTesting.cs
@@ -10,7 +10,11 @@ public class TileTesting : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        GetComponent<Tilemap>().SetTile(new Vector3Int(-1, 0, 0), tileTest);
+        GetComponent<Tilemap>().SetTile(new Vector3Int(0, -1, 0), tileTest);
         GetComponent<Tilemap>().SetTile(new Vector3Int(0, 0, 0), tileTest);
+        GetComponent<Tilemap>().SetTile(new Vector3Int(1, 0, 0), tileTest);
+        GetComponent<Tilemap>().SetTile(new Vector3Int(0, 1, 0), tileTest);
     }
 
     // Update is called once per frame
