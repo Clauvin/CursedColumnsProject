43522e000daa6ff09f4dc09d67e0d381fbe2e1d2
Cláuvin <Clauvin@users.noreply.github.com>
2022-12-13 17:04:52 -0300

    Created BlockPlacer (without implemented functions)

---
+++ Assets/Scripts/BlockPlacer.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class BlockPlacer : InterfaceBlockPlacer
+{
+    public bool AddBlock(Vector3Int position, TileBase tile)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool AddBlocks(Vector3Int[] positions, TileBase[] tiles)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
