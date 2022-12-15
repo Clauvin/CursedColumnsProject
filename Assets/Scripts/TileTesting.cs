5d5f5cbacfd4beedbd98ad09d87f3866574a372e
Cláuvin <Clauvin@users.noreply.github.com>
2022-12-15 18:56:24 -0300

    (#2) Emptying TileTesting of code

--- Assets/Scripts/TileTesting.cs
+++ Assets/Scripts/TileTesting.cs
@@ -10,11 +10,7 @@ public class TileTesting : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<Tilemap>().SetTile(new Vector3Int(-1, 0, 0), tileTest);
-        GetComponent<Tilemap>().SetTile(new Vector3Int(0, -1, 0), tileTest);
-        GetComponent<Tilemap>().SetTile(new Vector3Int(0, 0, 0), tileTest);
-        GetComponent<Tilemap>().SetTile(new Vector3Int(1, 0, 0), tileTest);
-        GetComponent<Tilemap>().SetTile(new Vector3Int(0, 1, 0), tileTest);
+
     }
 
     // Update is called once per frame
