3f864ea26e695ac62bc8c160fe38a56e2d620d14
Cláuvin <Clauvin@users.noreply.github.com>
2022-12-22 13:32:05 -0300

    (#3) In progress Block Remover class

---
+++ Assets/Scripts/BlockRemover.cs
@@ -0,0 +1,39 @@
+using ColumnsInterfaces;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockRemover : MonoBehaviour, InterfaceBlockRemover
+{
+    public bool EraseBlock(Vector3Int position)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool EraseBlocks(Vector3Int[] positions)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool RemoveBlock(Vector3Int position)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool RemoveBlocks(Vector3Int[] positions)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
