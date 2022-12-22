2cc6d1348fd6f245001e16ae139e8f57ce4b8477
Cláuvin <Clauvin@users.noreply.github.com>
2022-12-22 13:32:26 -0300

    (#3) Starting TestBlockRemover class

---
+++ Assets/Editor/Tests/TestBlockRemover.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using ColumnsInterfaces;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+using UnityEngine.Tilemaps;
+
+public class TestBlockRemover
+{
+    GameObject gridGameObject;
+    InterfaceBlockPlacer interfaceBlockPlacer;
+    Tile tileTest;
+    Tilemap tilemap;
+
+    [OneTimeSetUp]
+    public void NewTestSetUp()
+    {
+        SceneManager.LoadScene("TestScene");
+    }
+
+    [UnitySetUp]
+    public IEnumerator SetUp()
+    {
+        yield return new WaitForFixedUpdate();
+
+        Scene currentScene = SceneManager.GetActiveScene();
+        GameObject[] gameObjects = currentScene.GetRootGameObjects();
+
+        for (int i = 0; i < gameObjects.Length; i++)
+        {
+            if (gameObjects[i].name == "Grid")
+            {
+                gridGameObject = gameObjects[i];
+                break;
+            }
+        }
+
+        if (gridGameObject == null)
+        {
+            Assert.Fail();
+        }
+
+        yield return new EnterPlayMode();
+    }
+
+    [UnityTest]
+    public IEnumerator TestBlockRemoverEraseBlock()
+    {
+        InterfaceBlockPlacer interfaceBlockPlacer = gridGameObject.GetComponent<BlockPlacer>();
+        InterfaceBlockRemover interfaceBlockRemover = gridGameObject.GetComponent<BlockRemover>();
+        Tile tileTest = gridGameObject.GetComponentInChildren<TileTesting>().tileTest;
+        Tilemap tilemap = gridGameObject.GetComponentInChildren<Tilemap>();
+
+        interfaceBlockPlacer.AddBlock(new Vector3Int(0, 0, 0), tileTest);
+
+        
+
+        yield return new WaitForFixedUpdate();
+    }
+
+    [UnityTearDown]
+    public IEnumerator TearDown()
+    {
+        yield return new ExitPlayMode();
+    }
+}
