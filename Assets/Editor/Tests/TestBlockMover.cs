d44c2ff3f0bbb734cf401fb0183751b8e7d79c97
Cláuvin <Clauvin@users.noreply.github.com>
2022-12-24 19:39:09 -0300

    (#8) Finished TestBlockMovementTeleportBlock()

--- Assets/Editor/Tests/TestBlockMover.cs
+++ Assets/Editor/Tests/TestBlockMover.cs
@@ -1,25 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
+using ColumnsInterfaces;
 using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
+using UnityEngine.Tilemaps;
 
 public class TestBlockMovement
 {
-    // A Test behaves as an ordinary method
-    [Test]
-    public void TestBlockMovement()
+    GameObject gridGameObject;
+    InterfaceBlockPlacer interfaceBlockPlacer;
+    Tile tileTest;
+    Tilemap tilemap;
+
+    [OneTimeSetUp]
+    public void NewTestSetUp()
     {
-        // Use the Assert class to test conditions
+        SceneManager.LoadScene("TestScene");
+    }
+
+    [UnitySetUp]
+    public IEnumerator SetUp()
+    {
+        yield return new WaitForFixedUpdate();
+
+        Scene currentScene = SceneManager.GetActiveScene();
+        GameObject[] gameObjects = currentScene.GetRootGameObjects();
+
+        for (int i = 0; i < gameObjects.Length; i++)
+        {
+            if (gameObjects[i].name == "Grid")
+            {
+                gridGameObject = gameObjects[i];
+                break;
+            }
+        }
+
+        if (gridGameObject == null)
+        {
+            Assert.Fail();
+        }
+
+        yield return new EnterPlayMode();
     }
 
     // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
     // `yield return null;` to skip a frame.
     [UnityTest]
-    public IEnumerator TestBlockMovementWithEnumeratorPasses()
+    public IEnumerator TestBlockMovementTeleportBlock()
     {
-        // Use the Assert class to test conditions.
-        // Use yield to skip a frame.
-        yield return null;
+        InterfaceBlockPlacer interfaceBlockPlacer = gridGameObject.GetComponent<BlockPlacer>();
+        InterfaceBlockMover interfaceBlockMover = gridGameObject.GetComponent<BlockMover>();
+        Tile tileTest = gridGameObject.GetComponentInChildren<TileTesting>().tileTest;
+        Tilemap tilemap = gridGameObject.GetComponentInChildren<Tilemap>();
+
+        interfaceBlockMover.Init(tilemap);
+
+        interfaceBlockPlacer.AddBlock(new Vector3Int(0, 0, 0), tileTest);
+        interfaceBlockMover.TeleportBlock(new Vector3Int(0, 0, 0), new Vector3Int(3, 3, 0));
+
+        if (tilemap.GetTile(new Vector3Int(3, 3, 0)) == tileTest)
+        {
+            Assert.Pass();
+        }
+        else
+        {
+            Assert.Fail();
+        }
+        Assert.Fail();
+        yield return new WaitForFixedUpdate();
     }
 }
