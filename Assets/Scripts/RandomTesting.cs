9e8d250bca32aa798bcff3a5a0d536e30e684b5d
Cláuvin <Clauvin@users.noreply.github.com>
2022-12-24 21:04:29 -0300

    (#8) Testing position of tiles

--- Assets/Scripts/RandomTesting.cs
+++ Assets/Scripts/RandomTesting.cs
@@ -8,7 +8,8 @@ public class RandomTesting : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(GetComponent<Tilemap>().GetTile(new Vector3Int(0, 0, 0)));
+        GetComponent<Tilemap>().SetTile(new Vector3Int(0, 3, 0), GetComponent<TileTesting>().tileTest);
+        GetComponent<Tilemap>().SetTile(new Vector3Int(0, 0, 0), GetComponent<TileTesting>().tileTest);
     }
 
     // Update is called once per frame
