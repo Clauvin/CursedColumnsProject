b713bef84c29539b41a71e0c9b08095025599f20
Cláuvin <Clauvin@users.noreply.github.com>
2023-01-06 17:52:46 -0300

    (#11) Created TestDataManager.cs

---
+++ Assets/Editor/Tests/TestDataManager.cs
@@ -0,0 +1,64 @@
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+public class TestDataManager : MonoBehaviour
+{
+    DataManager dataManager;
+
+    [OneTimeSetUp]
+    public void NewTestSetUp()
+    {
+        SceneManager.LoadScene("TestScene");
+    }
+
+    [UnitySetUp]
+    public IEnumerator SetUp()
+    {
+        yield return new WaitForFixedUpdate();
+
+        Scene currentScene = SceneManager.GetActiveScene();
+        GameObject[] gameObjects = currentScene.GetRootGameObjects();
+
+        for (int i = 0; i < gameObjects.Length; i++)
+        {
+            if (gameObjects[i].name == "DataManager")
+            {
+                dataManager = gameObjects[i].GetComponent<DataManager>();
+                break;
+            }
+        }
+
+        if (dataManager == null)
+        {
+            Assert.Fail();
+        }
+
+        yield return new EnterPlayMode();
+
+    }
+
+    [UnityTest]
+    public IEnumerator TestDataManagerStart()
+    {
+        yield return new WaitForSeconds(2);
+        
+
+        if (dataManager.gameTimer.GetTimer() > 0)
+        {
+            Assert.Pass();
+        } 
+        else
+        {
+            Debug.Log(dataManager.gameTimer.GetTimer());
+            Assert.Fail();
+        }
+
+        yield return new WaitForFixedUpdate();
+    }
+
+
+}
