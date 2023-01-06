b4abe2485bf34199be8069dcf4bbf2395dba140d
Cláuvin <Clauvin@users.noreply.github.com>
2023-01-06 17:29:42 -0300

    (#11) Created DataManager.cs

---
+++ Assets/Scripts/DataManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+    public Vector2Int blockSetSpawn;
+    public Vector2Int gameSpace;
+    public GameTimer gameTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
