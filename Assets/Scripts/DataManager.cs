2a309aa745a680202a7db1f3250c436ed0488f30
Cláuvin <Clauvin@users.noreply.github.com>
2023-01-07 20:28:27 -0300

    (#13) Added bool isPaused to DataManager

--- Assets/Scripts/DataManager.cs
+++ Assets/Scripts/DataManager.cs
@@ -7,11 +7,12 @@ public class DataManager : MonoBehaviour
     public Vector2Int blockSetSpawn;
     public Vector2Int gameSpace;
     public GameTimer gameTimer;
+    public bool isPaused;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        isPaused = false;
     }
 
     // Update is called once per frame
