be60e4352fb9f6cb0b0e7cfb60e642c319277fa1
Cláuvin <Clauvin@users.noreply.github.com>
2023-01-07 20:06:41 -0300

    (#13) PlayerInputs now is PlayerInteractions

---
+++ Assets/Scripts/PlayerInteractions.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteractions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void ReceiveInput()
+    {
+
+    }
+
+    private bool MoveBlockLaterally()
+    {
+        return false;
+    }
+
+    private void FastBlockDrop()
+    {
+
+    }
+
+    private void InstantBlockDrop()
+    {
+
+    }
+
+    private void PauseGame()
+    {
+
+    }
+
+    private void ExitGame()
+    {
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
