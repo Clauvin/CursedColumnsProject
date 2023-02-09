de84ceb8f92510bc9df43d4850fe97850d86d78e
Cláuvin <Clauvin@users.noreply.github.com>
2023-02-09 12:06:59 -0300

    (#11) Created Current and NextBlockSet functions

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using BlockSets;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,6 +10,9 @@ public class GameManager : MonoBehaviour
     public static PlayerInteractions inputManager;
     public static GameTimer gameTimer;
     public static BlockManipulator blockManipulator;
+
+    public static BlockSet currentBlockSet;
+    public static BlockSet nextBlockSet;
 
     void Awake()
     {
@@ -27,6 +31,8 @@ public class GameManager : MonoBehaviour
         blockManipulator.StartManager();
 
         CreateStartingGameSpace();
+        CreateCurrentBlockSet();
+        CreateNextBlockSet();
     }
 
     public void CreateStartingGameSpace()
@@ -50,9 +56,33 @@ public class GameManager : MonoBehaviour
         }
 
         Vector3Int[] blocksToAdd = newBlocks.ToArray();
-        Tile tileToUse = DataManager.commonTiles[0];
+        Tile tileToUse = DataManager.unpassableTile;
 
         blockManipulator.GetBlockPlacer().AddSameBlockMultiplesTimes(blocksToAdd, tileToUse);
+    }
+
+    public void CreateCurrentBlockSet()
+    {
+        //For 0.3, change commonTiles[0] to commonTiles[random], and add random tiles.
+        Tile tile = DataManager.commonTiles[0];
+        List<Vector3Int> positions = new List<Vector3Int>();
+        positions.Add(new Vector3Int(0, 0, 0));
+        positions.Add(new Vector3Int(0, 1, 0));
+        positions.Add(new Vector3Int(0, 2, 0));
+
+        currentBlockSet = new BlockSet(tile, positions);
+    }
+
+    public void CreateNextBlockSet()
+    {
+        //For 0.3, change commonTiles[0] to commonTiles[random], and add random tiles.
+        Tile tile = DataManager.commonTiles[0];
+        List<Vector3Int> positions = new List<Vector3Int>();
+        positions.Add(new Vector3Int(0, 0, 0));
+        positions.Add(new Vector3Int(0, 1, 0));
+        positions.Add(new Vector3Int(0, 2, 0));
+
+        nextBlockSet = new BlockSet(tile, positions);
     }
 
     // Update is called once per frame
