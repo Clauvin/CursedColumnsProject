6c057502567689a358c03425e9d24e4e62026a3f
Cláuvin <Clauvin@users.noreply.github.com>
2023-03-19 11:11:23 -0300

    (#22) Created TestUIManager.cs

---
+++ Assets/Editor/Tests/TestUIManager.cs
@@ -0,0 +1,62 @@
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+public class TestUIManager : MonoBehaviour
+{
+    GameObject gameManager;
+
+    [OneTimeSetUp]
+    public void NewTestSetUp()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    [UnitySetUp]
+    public IEnumerator SetUp()
+    {
+        yield return new WaitForFixedUpdate();
+
+        Scene currentScene = SceneManager.GetActiveScene();
+        GameObject[] gameObjects = currentScene.GetRootGameObjects();
+
+        for (int i = 0; i < gameObjects.Length; i++)
+        {
+            Debug.Log(gameObjects[i].name);
+            if (gameObjects[i].name == "GameManager")
+            {
+                gameManager = gameObjects[i];
+                break;
+            }
+        }
+
+        if (gameManager == null)
+        {
+            Assert.Fail();
+        }
+
+        yield return new EnterPlayMode();
+    }
+
+    [UnityTest]
+    public IEnumerator TestPause()
+    {
+        UIManager uiManager = gameManager.GetComponent<UIManager>();
+
+        UIManager.Pause();
+
+        if (!UIManager.pauseText.enabled)
+        {
+            Assert.Fail();
+        }
+        else
+        {
+            Assert.Pass();
+        }
+
+        yield return new WaitForFixedUpdate();
+    }
+}
