05ebc56f390538415efe73c6f92e44360d796612
Cláuvin <Clauvin@users.noreply.github.com>
2023-03-23 17:12:43 -0300

    (#22) Checks work on all directions, but I still need to avoid same matches being counted more than once

--- Assets/Scripts/BlockMatchChecker.cs
+++ Assets/Scripts/BlockMatchChecker.cs
@@ -7,10 +7,15 @@ using UnityEngine.Tilemaps;
 public class BlockMatchChecker : MonoBehaviour
 {
     public Tilemap tileMap;
+    public int matchSize = 3;
     private Tilemap horizontalCheckTilemap;
     private Tilemap verticalCheckTilemap;
     private Tilemap leftDownCheckTilemap;
     private Tilemap rightDownCheckTilemap;
+    private List<MatchSet> horizontalMatchSetsFound;
+    private List<MatchSet> verticalMatchSetsFound;
+    private List<MatchSet> leftDownMatchSetsFound;
+    private List<MatchSet> rightDownMatchSetsFound;
 
     // Start is called before the first frame update
     void Start()
@@ -29,8 +34,14 @@ public class BlockMatchChecker : MonoBehaviour
         verticalCheckTilemap = tileMap;
         leftDownCheckTilemap = tileMap;
         rightDownCheckTilemap = tileMap;
+        horizontalMatchSetsFound = new List<MatchSet>();
+        verticalMatchSetsFound = new List<MatchSet>();
+        leftDownMatchSetsFound = new List<MatchSet>();
+        rightDownMatchSetsFound = new List<MatchSet>();
         HorizontalChecks();
-
+        VerticalChecks();
+        LeftDownChecks();
+        RightDownChecks();
     }
 
     private void HorizontalChecks()
@@ -44,38 +55,140 @@ public class BlockMatchChecker : MonoBehaviour
                 Tile tile = horizontalCheckTilemap.GetTile<Tile>(new Vector3Int(i, j, 0));
                 if (tile != null && tile != DataManager.unpassableTile)
                 {
-                    MatchSet matchSet = new MatchSet(tile, new List<Vector3Int>(), MatchSet.Direction.HORIZONTAL);
-                    matchSet.positions.Add(new Vector3Int(i, j, 0));
+                    MatchSet matchSet = new MatchSet(MatchSet.Direction.HORIZONTAL);
+                    matchSet.AddTile(tile, new Vector3Int(i, j, 0));
 
-                    matchSet = 
+                    matchSet = HorizontalDynamicDoubleCheck(matchSet);
+                    if (matchSet.positions.Count >= matchSize)
+                    {
+                        horizontalMatchSetsFound.Add(matchSet);
+                    }
+                    
                 }
             }
         }
-        
-        /*
-         * iterate through the playable area
-         * for each block, check to the left and the right if there are similar blocks
-         * if you found blocks or not from the color of the starting block, remove those from the horizontalCheckTilemap
-         * if it found, store the match in the horizontal match list
-         */
     }
 
     private MatchSet HorizontalDynamicDoubleCheck(MatchSet startingMatchSet)
     {
-        MatchSet endingMatchSet = DynamicCheck(startingMatchSet, startingMatchSet.positions[0], new Vector3Int(-1, 0, 0));
-        endingMatchSet = DynamicCheck(endingMatchSet, startingMatchSet.positions[0], new Vector3Int(1, 0, 0));
+        MatchSet endingMatchSet = DynamicCheck(startingMatchSet, startingMatchSet.positions[0], new Vector3Int(-1, 0, 0),
+            horizontalCheckTilemap);
+        endingMatchSet = DynamicCheck(endingMatchSet, startingMatchSet.positions[0], new Vector3Int(1, 0, 0),
+            horizontalCheckTilemap);
         return endingMatchSet;
     }
 
-    private MatchSet DynamicCheck(MatchSet matchSet, Vector3Int currentPosition, Vector3Int toWhere)
+    private void VerticalChecks()
+    {
+        Vector2 gameSpace = GameManager.dataManager.gameSpace;
+
+        for (int i = GameManager.dataManager.leftLimit + 1; i < GameManager.dataManager.rightLimit; i++)
+        {
+            for (int j = GameManager.dataManager.lowerLimit + 1; j < GameManager.dataManager.upperLimit; j++)
+            {
+                Tile tile = verticalCheckTilemap.GetTile<Tile>(new Vector3Int(i, j, 0));
+                if (tile != null && tile != DataManager.unpassableTile)
+                {
+                    MatchSet matchSet = new MatchSet(MatchSet.Direction.VERTICAL);
+                    matchSet.AddTile(tile, new Vector3Int(i, j, 0));
+
+                    matchSet = VerticalDynamicDoubleCheck(matchSet);
+                    if (matchSet.positions.Count >= matchSize)
+                    {
+                        verticalMatchSetsFound.Add(matchSet);
+                    }
+
+                }
+            }
+        }
+    }
+
+    private MatchSet VerticalDynamicDoubleCheck(MatchSet startingMatchSet)
+    {
+        MatchSet endingMatchSet = DynamicCheck(startingMatchSet, startingMatchSet.positions[0], new Vector3Int(0, -1, 0),
+            verticalCheckTilemap);
+        endingMatchSet = DynamicCheck(endingMatchSet, startingMatchSet.positions[0], new Vector3Int(0, 1, 0),
+            verticalCheckTilemap);
+        return endingMatchSet;
+    }
+
+    private void LeftDownChecks()
+    {
+        Vector2 gameSpace = GameManager.dataManager.gameSpace;
+
+        for (int i = GameManager.dataManager.leftLimit + 1; i < GameManager.dataManager.rightLimit; i++)
+        {
+            for (int j = GameManager.dataManager.lowerLimit + 1; j < GameManager.dataManager.upperLimit; j++)
+            {
+                Tile tile = leftDownCheckTilemap.GetTile<Tile>(new Vector3Int(i, j, 0));
+                if (tile != null && tile != DataManager.unpassableTile)
+                {
+                    MatchSet matchSet = new MatchSet(MatchSet.Direction.DOWNLEFT);
+                    matchSet.AddTile(tile, new Vector3Int(i, j, 0));
+
+                    matchSet = LeftDownDynamicDoubleCheck(matchSet);
+                    if (matchSet.positions.Count >= matchSize)
+                    {
+                        leftDownMatchSetsFound.Add(matchSet);
+                    }
+                }
+            }
+        }
+    }
+
+    private MatchSet LeftDownDynamicDoubleCheck(MatchSet startingMatchSet)
+    {
+        MatchSet endingMatchSet = DynamicCheck(startingMatchSet, startingMatchSet.positions[0], new Vector3Int(-1, -1, 0),
+            leftDownCheckTilemap);
+        endingMatchSet = DynamicCheck(endingMatchSet, startingMatchSet.positions[0], new Vector3Int(1, 1, 0),
+            leftDownCheckTilemap);
+        return endingMatchSet;
+    }
+
+    private void RightDownChecks()
+    {
+        Vector2 gameSpace = GameManager.dataManager.gameSpace;
+
+        for (int i = GameManager.dataManager.leftLimit + 1; i < GameManager.dataManager.rightLimit; i++)
+        {
+            for (int j = GameManager.dataManager.lowerLimit + 1; j < GameManager.dataManager.upperLimit; j++)
+            {
+                Tile tile = rightDownCheckTilemap.GetTile<Tile>(new Vector3Int(i, j, 0));
+                if (tile != null && tile != DataManager.unpassableTile)
+                {
+                    MatchSet matchSet = new MatchSet(MatchSet.Direction.DOWNRIGHT);
+                    matchSet.AddTile(tile, new Vector3Int(i, j, 0));
+
+                    matchSet = RightDownDynamicDoubleCheck(matchSet);
+                    if (matchSet.positions.Count >= matchSize)
+                    {
+                        rightDownMatchSetsFound.Add(matchSet);
+                    }
+                }
+            }
+        }
+    }
+
+    private MatchSet RightDownDynamicDoubleCheck(MatchSet startingMatchSet)
+    {
+        MatchSet endingMatchSet = DynamicCheck(startingMatchSet, startingMatchSet.positions[0], new Vector3Int(1, -1, 0),
+            rightDownCheckTilemap);
+        endingMatchSet = DynamicCheck(endingMatchSet, startingMatchSet.positions[0], new Vector3Int(-1, 1, 0),
+            rightDownCheckTilemap);
+        return endingMatchSet;
+    }
+
+    private MatchSet DynamicCheck(MatchSet matchSet, Vector3Int currentPosition, Vector3Int toWhere, Tilemap tileMap)
     {
         Vector3Int positionToCheck = currentPosition + toWhere;
         //Change this part to get the correct tileset instead of the horizontal one
-        if (matchSet.tiles[0] == horizontalCheckTilemap.GetTile<Tile>(positionToCheck))
+        if (matchSet.tiles[0] == tileMap.GetTile<Tile>(positionToCheck))
         {
-            //matchSet.
-            //return 
+            matchSet.AddTile(matchSet.tiles[0], currentPosition);
+            return DynamicCheck(matchSet, positionToCheck, toWhere, tileMap);
         }
+
+        return matchSet;
     }
 
     private void VerticalCheck()
