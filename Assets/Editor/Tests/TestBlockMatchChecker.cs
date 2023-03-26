57b44d4259488522720db7db82ffd39acd5fbf61
Cláuvin <Clauvin@users.noreply.github.com>
2023-03-26 10:54:46 -0300

    (#22) Created TestBlockMatchChecker.cs (but the TestHorizontalCheck() is failing)

---
+++ Assets/Editor/Tests/TestBlockMatchChecker.cs
@@ -0,0 +1,67 @@
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+using UnityEngine.Tilemaps;
+
+[TestFixture]
+public class TestBlockMatchChecker
+{
+    GameObject gameManagerPrefab;
+    GameManager gameManager;
+    BlockMatchChecker blockMatchChecker;
+
+    [OneTimeSetUp]
+    public void NewTestSetUp()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    [UnitySetUp]
+    public IEnumerator SetUp()
+    {
+        yield return new WaitForFixedUpdate();
+
+        gameManagerPrefab = (GameObject)Resources.Load("TestResources/GameManager");
+        gameManager = gameManagerPrefab.GetComponent<GameManager>();
+        gameManager.SetUpStaticManagers();
+        blockMatchChecker = gameManagerPrefab.GetComponent<BlockMatchChecker>();
+
+        yield return new WaitForFixedUpdate();
+     }
+
+    [UnityTest]
+    public IEnumerator TestHorizontalCheck()
+    {
+        Tilemap tilemap = blockMatchChecker.tileMap;
+
+        List<Vector3Int> positionsList = new List<Vector3Int>();
+        positionsList.Add(new Vector3Int(-1, 0, 0));
+        positionsList.Add(new Vector3Int(0, 0, 0));
+        positionsList.Add(new Vector3Int(1, 0, 0));
+
+        Tile tile = DataManager.commonTiles[0];
+
+        BlockPlacer.AddSameBlockMultiplesTimesInATilemap(positionsList.ToArray(), tile, tilemap);
+
+        blockMatchChecker.HorizontalChecks();
+
+        if (blockMatchChecker.HorizontalMatchSetsFound.Count != 1)
+        {
+            Assert.Fail();
+        } 
+        
+        Assert.Pass();
+
+        yield return new WaitForFixedUpdate();
+
+    }
+
+    [UnityTearDown]
+    public IEnumerator TearDown()
+    {
+        yield return new ExitPlayMode();
+    }
+}
