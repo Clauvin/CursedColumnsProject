9f23d750475d17ad223eea638e9c845314aea83d
Cláuvin <Clauvin@users.noreply.github.com>
2023-04-14 17:16:10 -0300

    (#28) Fixed name of timeOfDelayAfterAllMatchChecks variable

--- Assets/Scripts/DataManager.cs
+++ Assets/Scripts/DataManager.cs
@@ -17,7 +17,7 @@ public class DataManager : MonoBehaviour
     public float secondsOfDelayAfterAllMatchChecks;
     public float timeWithoutInputCheck;
     public float timeForDelayAfterAllMatchChecks;
-    public bool timeToAddDelayAfterMatchCheck;
+    public bool timeOfDelayAfterAllMatchChecks;
 
     public float timeWithoutPBlockGravityBeingApplied;
 
