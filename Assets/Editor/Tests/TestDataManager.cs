538a429cc7f8ad3460a0023976ce012e82b8dcd6
Cláuvin <Clauvin@users.noreply.github.com>
2023-04-18 19:46:24 -0300

    (#22) Refactored TestDataManager.cs

--- Assets/Editor/Tests/TestDataManager.cs
+++ Assets/Editor/Tests/TestDataManager.cs
@@ -16,6 +16,8 @@ public class TestDataManager : MonoBehaviour
     {
         dataManager = new DataManager();
 
+        dataManager.StartManager();
+
         yield return new EnterPlayMode();
 
     }
@@ -23,8 +25,6 @@ public class TestDataManager : MonoBehaviour
     [UnityTest]
     public IEnumerator TestDataManagerInitErrorStrings()
     {
-        dataManager.StartManager();
-
         if ((DataManager.errorMessageDidNotLoadUnpassableTile == "Couldn't load unpassable tile.") &&
                 (DataManager.errorMessageDidNotLoadCommonTiles == "Couldn't load common tiles.") &&
                 (DataManager.errorMessageDidNotLoadAllCommonTiles == "Not all common tiles were loaded."))
@@ -42,8 +42,6 @@ public class TestDataManager : MonoBehaviour
     [UnityTest]
     public IEnumerator TestDataManagerInitUnpassableTile()
     {
-        dataManager.StartManager();
-
         AsyncOperationHandle<Tile> opHandle;
         Tile unpassableTile;
 
@@ -67,8 +65,6 @@ public class TestDataManager : MonoBehaviour
     [UnityTest]
     public IEnumerator TestDataManagerInitCommonTiles()
     {
-        dataManager.StartManager();
-
         AsyncOperationHandle<System.Collections.Generic.IList<Tile>> opHandle;
         Tile[] commonTiles;
 
@@ -95,8 +91,6 @@ public class TestDataManager : MonoBehaviour
     [UnityTest]
     public IEnumerator TestDifficultyScoreValuesInitialization()
     {
-        dataManager.StartManager();
-
         for (int i = 0; i < dataManager.difficultyScoreValues.Count; i++)
         {
             if (dataManager.difficultyScoreValues[i] == 0)
@@ -113,8 +107,6 @@ public class TestDataManager : MonoBehaviour
     [UnityTest]
     public IEnumerator TestBlockGravityDiffValuesInitialization()
     {
-        dataManager.StartManager();
-
         for (int i = 0; i < dataManager.blockGravityDiffValues.Length; i++)
         {
             if (dataManager.blockGravityDiffValues[i] == 0)
@@ -131,8 +123,6 @@ public class TestDataManager : MonoBehaviour
     [UnityTest]
     public IEnumerator TestPlayerInputTimeDiffValuesInitialization()
     {
-        dataManager.StartManager();
-
         for (int i = 0; i < dataManager.playerInputTimeDiffValues.Length; i++)
         {
             if (dataManager.playerInputTimeDiffValues[i] == 0)
@@ -149,8 +139,6 @@ public class TestDataManager : MonoBehaviour
     [UnityTest]
     public IEnumerator TestDelayAfterMatchDiffValuesInitialization()
     {
-        dataManager.StartManager();
-
         for (int i = 0; i < dataManager.delayAfterMatchDiffValues.Length; i++)
         {
             if (dataManager.delayAfterMatchDiffValues[i] == 0)
